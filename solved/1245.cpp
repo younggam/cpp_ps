431a92814c2f9486484583d27d6ef3d4e4c893ad
younggam <nainjoo2001@naver.com>
2024-07-14 17:58:17 +0900

    1245

--- solved/1245.cpp
+++ solved/1245.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-// #include <fstream>
 
 using namespace std;
 
@@ -7,7 +6,7 @@ double solve(int *masses, int *xes, int N, int i) {
     double left_x = xes[i - 1];
     double right_x = xes[i];
     double x = (left_x + right_x) * 0.5;
-    while (true) {
+    while (abs(right_x - left_x) >= 1E-12) {
         double left_f = 0;
         for (int k = 0; k < i; k++) {
             left_f += masses[k] / ((x - xes[k]) * (x - xes[k]));
@@ -16,7 +15,6 @@ double solve(int *masses, int *xes, int N, int i) {
         for (int k = i; k < N; k++) {
             right_f += masses[k] / ((x - xes[k]) * (x - xes[k]));
         }
-        if (abs(right_x - left_x) < 1E-12) break;
         if (left_f > right_f) {
             double temp = x;
             x = (x + right_x) * 0.5;
@@ -31,9 +29,6 @@ double solve(int *masses, int *xes, int N, int i) {
 }
 
 int main(int argc, char **argv) {
-    // ifstream cin("../input.txt");
-    // ifstream answer("../output.txt");
-
     int T, t = 1;
     cin >> T;
     cout << fixed;
@@ -47,15 +42,8 @@ int main(int argc, char **argv) {
         for (int j = 0; j < N; j++) cin >> masses[j];
 
         cout << '#' << t;
-        // string skip;
-        // answer >> skip;
         for (int j = 1; j < N; j++) {
-            double result = solve(masses, xes, N, j);
-            cout << ' ' << result;
-            // double a;
-            // answer >> a;
-            // if (result == a) cout << " success";
-            // else cout << ' ' << a << " != " << result;
+            cout << ' ' <<  solve(masses, xes, N, j);
         }
         cout << endl;
 
@@ -65,11 +53,3 @@ int main(int argc, char **argv) {
     }
     return 0;
 }
-
-// cout << '#' << t << ' '  << endl;
-//
-// short a;
-// string skip;
-// answer >> skip >> a;
-// if (min == a) cout << "success" << endl;
-// else cout << a << " != " << min << endl;
